0dce29dab6baa0c0ce77cca8b4278db25881180b
2021330 <2021330@denshi.tohoku-pc.local>
2021-05-18 13:17:45 +0900

    2nd commit

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
+const SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+const port = new SerialPort('COM1',{
+    baudRate: 115200
+});
+port.pipe(parser);
 
 app.get('/' , function(req, res){
     res.sendFile(__dirname+'/index.html');
@@ -12,9 +21,23 @@ io.on('connection',function(socket){
     socket.on('message',function(msg){
         console.log('message: ' + msg);
         io.emit('message', msg);
+        port.write(msg+'\n', (err, results) => {
+            if(err) {
+                console.log('Err: '+err);
+                console.log('Results: '+results);
+            }
+        });
     });
 });
 
 http.listen(PORT, function(){
     console.log('server listening. Port:' + PORT);
+});
+
+port.on('open', () => {
+    console.log('port open');
+});
+
+parser.on('data', (data) => {
+    console.log(data);
 });
